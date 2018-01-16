0c51eec3636c10b0c45c3452c127368f90b3f02e
Amanda Xu <kayeexu@gmail.com>
2018-01-16 02:06:52 -0800

    removed unused files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         <meta name="user_id" content="{{Auth::user()->id}}">
     @endif
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Notes') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -33,7 +33,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{ config('app.name', 'Notes') }}
                     </a>
                 </div>
 
